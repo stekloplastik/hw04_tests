bbcc013a99a17265627f4e875e188d8fab71eb04
Николай <slonopatam52@gmail.com>
2021-02-09 23:57:47 +0300

    1st review(PEP2)

--- posts/urls.py
+++ posts/urls.py
@@ -11,9 +11,6 @@ urlpatterns = [
     path('<str:username>/', views.profile, name='profile'),
     # Просмотр записи
     path('<str:username>/<int:post_id>/', views.post_view, name='post'),
-    path(
-        '<str:username>/<int:post_id>/edit/',
-        views.post_edit,
-        name='post_edit'
-         ),
+    path('<str:username>/<int:post_id>/edit/', views.post_edit,
+         name='post_edit'),
 ]
